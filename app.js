618810b8e1927066bac85f433593c7133cda98f1
HamzaSaeed06 <hamzasaeed0619@gmail.com>
2025-09-06 19:21:15 -0700

    add more functionality

--- app.js
+++ app.js
@@ -2,47 +2,74 @@ var display = document.getElementById("display");
 var after = document.getElementById("result");
 var getIdTag = document.getElementById("equal");
 var insertTag = '<i class="fa-solid fa-equals fa-2xs"></i>';
-var z = "";
+var addDisplayValue = "";
 
-function getValue(value, b) {
+function getValue(value){
+
   var result = display.value.slice(display.value.length - 1);
   var operators = ["+", "-", "/", "*", "%"];
-  if (operators.indexOf(result) !== -1 && operators.indexOf(value) !== -1) {  // <===== it check the last value of the display if the last value of the display is any operator it change with new input operator from the user
+  var checKDot = display.value.slice(display.value.length -1);
+  var dot = ["."];
+
+  if(operators.indexOf(result) !== -1 && operators.indexOf(value) !== -1) {  // <===== it check the last value of the display if the last value of the display is any operator it change with new input operator from the user
+    
+    if(display.value.slice(0, 1) === "-" && display.value.slice(1,2) === ""){
+      display.value = display.value.slice(0, 1);z
+    }else{
+      display.value = display.value.slice(0, -1) + value;
+    }
+
+  }
+  else if(dot.indexOf(checKDot) !== -1 && dot.indexOf(value) !== -1){
     display.value = display.value.slice(0, -1) + value;
-  } else {                                                      // <====== if the last value id not any operator then Else is run
+  }
+   else {                                                      // <====== if the last value id not any operator then Else is run
+   
     var firstValueOperators = ["+", "/", "*", "%"];
     var ifFirstFound = false;
-    z += value;
-    console.log(z);
-    var a = z.split("");
+    addDisplayValue += value;
+    var makeArray = addDisplayValue.split("");
+    
     for (var i = 0; i < firstValueOperators.length; i++) {      // <====== it check the first operator if first operator is exist between (/, *, +, %) in the display then it delete the first index
-      var firstOperatorsCheck = a.slice(0, 1).includes(firstValueOperators[i]);
+      var firstOperatorsCheck = makeArray.slice(0, 1).includes(firstValueOperators[i]);
       if (firstOperatorsCheck) {
         ifFirstFound = firstOperatorsCheck;
-        console.log(ifFirstFound);
       }
     }
 
     if (ifFirstFound) {    // <====== if the first index is operator in the index 0 is found so this condition delete the value
-      a.shift();
-      console.log(a);
-      var b = a.join("");
-      if (b) {
-        display.value = b;
-        console.log(b);
-        console.log(eval(b));
+      makeArray.shift();
+      var makeString = makeArray.join("");
+      if (makeString) {
+        display.value = makeString;
       }
     } else {               // <====== if the first index is not operator so then Else is run
       display.value += value;
     }
-    z = display.value;
+
+    addDisplayValue = display.value;
 
     for (var i = 0; i < operators.length; i++) {   // <====== it check if the any operator is exist between 2 operands then it print the result therwise not
       var check = display.value.includes(operators[i]);
       if (check) {
-        after.value = "";
-        after.value += eval(display.value);
-        getIdTag.innerHTML = insertTag;
+
+        var isFound = false;
+        for(var i = 0; i < operators.length; i++){
+        var checkOpt = display.value.slice(display.value.length - 1).includes(operators[i]);
+        if(checkOpt){
+          checkOpt = isFound
+        }
+        }
+
+        if(isFound && after.value !== ""){
+          display.value = display.value.slice(0, -1);
+          getIdTag.innerHTML = insertTag;
+          after.value = eval(display.value)
+        }else{
+          after.value = eval(display.value);
+          getIdTag.innerHTML = insertTag;
+        }
+
       }
     }
   }
@@ -52,28 +79,53 @@ function removeAll() {
   result.value = "";
   getIdTag.innerHTML = "";
   display.value = "";
-  z = "";
+  addDisplayValue = "";
 }
 
 function deleteLeft() {
+
   getIdTag.innerHTML = "";
   display.value = display.value.slice(0, -1);
+  addDisplayValue = addDisplayValue.slice(0, -1);
   result.value = "";
-  console.log(display.value);
   var operators = ["+", "-", "/", "*", "%"];
-  for (var i = 0; i < operators.length; i++) {
+
+  for (var i = 0; i < operators.length; i++) {   // <====== it check if the any operator is exist between 2 operands then it print the result therwise not  
     var check = display.value.includes(operators[i]);
-    if (check) {
-      after.value = "";
-      after.value += eval(display.value);
-      getIdTag.innerHTML = insertTag;
+
+      if (check) {
+        console.log(display.value)
+        getIdTag.innerHTML = insertTag;
+        var isFound = false;
+
+        for(var i = 0; i < operators.length; i++){
+        var checkOpt = display.value.slice(display.value.length - 1).includes(operators[i]);
+        if(checkOpt){
+          isFound = true;
+          console.log(isFound)
+        }
+        }
+
+        if(isFound){
+          after.value = eval(display.value.slice(0, -1))
+          console.log(display.value)
+          if(display.value.slice(0, 1) === "-" && display.value.slice(1, 1) === "" && display.value.slice(1, 2) === "" ){
+            after.value = "";
+            getIdTag.innerHTML = "";
+             console.log(display.value)
+          }
+        }
+        else{
+          after.value = eval(display.value)
+          console.log(display.value)
+        }
+
+      }
     }
-  }
 }
 
 function calculation() {
-  display.value = "";
-  display.value += after.value;
-  after.value = "";
+  display.value = eval(display.value);
+  after.value = ""
   getIdTag.innerHTML = "";
 }
